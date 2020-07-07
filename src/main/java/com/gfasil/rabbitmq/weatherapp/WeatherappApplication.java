3848d0cc8ade4fabf005e61e4cbbbd9a83e4aade
gfasil <fhabtegiorgis@mum.edu>
2020-07-07 12:15:26 -0500

    first commit

---
+++ src/main/java/com/gfasil/rabbitmq/weatherapp/WeatherappApplication.java
@@ -0,0 +1,25 @@
+package com.gfasil.rabbitmq.weatherapp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableRabbit
+public class WeatherappApplication {
+
+    @Value("${queue.weather.name}")
+    private String weatherQueue;
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherappApplication.class, args);
+    }
+    @Bean
+    public Queue queue(){
+        return new Queue(weatherQueue,true);
+    }
+
+
+}
